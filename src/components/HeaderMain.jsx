15f8d0e4c850e33a01735e4fc26d024d99e5a415
Christian Peraza <christianperaza2000@gmail.com>
2024-07-11 20:27:54 -0300

    Header Main Component Created

---
+++ src/components/HeaderMain.jsx
@@ -0,0 +1,11 @@
+export const HeaderMain = () => {
+    return (
+        <div id="headerMain-container">
+            <h1 id="headerMain-logo">Logo</h1>
+
+            <nav id="headerMain-nav">
+                <h2>My List</h2>
+            </nav>
+        </div>
+    )
+}
