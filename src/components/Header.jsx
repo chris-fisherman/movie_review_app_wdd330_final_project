2f40da3de3bd78f934d549a86c890de08e361dc3
Christian Peraza <christianperaza2000@gmail.com>
2024-07-11 20:53:19 -0300

    Header.css File Created and Imported

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,7 @@
 import { HeaderMain } from "./HeaderMain"
 
+import '../css/Header.css'
+
 export const Header = () => {
     return (
         <header id="header">
--- src/components/HeaderMain.jsx
+++ src/components/HeaderMain.jsx
@@ -1,3 +1,5 @@
+import '../css/Header.css'
+
 export const HeaderMain = () => {
     return (
         <div id="headerMain-container">
