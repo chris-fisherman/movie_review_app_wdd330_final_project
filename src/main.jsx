ee46dae1f392983efac21472eeb00e7a2e8699e0
Christian Peraza <christianperaza2000@gmail.com>
2024-07-11 20:58:18 -0300

    index.css Name Changed

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom/client'
 import { Header } from './components/Header.jsx'
 import App from './App.jsx'
 
-import './css/index.css'
+import './css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
