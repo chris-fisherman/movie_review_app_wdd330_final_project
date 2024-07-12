6dec7a7bda65d22c18a8b4ccd366d17421e5df82
Christian Peraza <christianperaza2000@gmail.com>
2024-07-12 20:57:20 -0300

    Routes Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import './css/App.css'
 
 function App() {
   return (
-    <>
-      <p>App</p>
-    </>
+    <div id='App'>
+      <Router>
+        <Routes>
+          <Route index element={<h1>Home Page</h1>} />
+          <Route path="review/:id" element={<h1>Detail Page Review</h1>} />
+          <Route path="movies/:type" element={<h1>Top 100 Movies</h1>} />
+          <Route path="series/:type" element={<h1>Top 100 Series</h1>} />
+          <Route path="myList" element={<h1>My List</h1>} />
+          <Route path="/*" element={<h1>Error Page</h1>} />
+        </Routes>
+      </Router>
+    </div>
   )
 }
 
