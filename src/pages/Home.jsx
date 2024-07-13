936ed2480ee8a251540ec6d31868fc6c07d84685
Christian Peraza <christianperaza2000@gmail.com>
2024-07-12 22:47:13 -0300

    Fetch Popular Movies Init

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,17 @@
-import React from "react"
+import React, { useEffect } from "react"
 
 import "../css/Home.css"
 
 export const Home = () => {
+    
+    useEffect(() => {
+        fetch('https://api.themoviedb.org/3/movie/popular?api_key=3635f4a229260177fe2b5498e1ba967e&language=en-US')
+        .then(response => response.json())
+        .then(data => console.log(data))
+    }, [])
+      
+      
+
     return (
         <>
             Home Page
