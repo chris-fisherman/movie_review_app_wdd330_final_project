ae4bb92182c6e9d537d30c3c8fc3afe1398f76a9
Christian Peraza <christianperaza2000@gmail.com>
2024-07-12 21:56:32 -0300

    Simple Nav Styles Added

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,17 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
+import '../css/Nav.css'
+
 export const Nav = () => {
     return (
         <nav id="nav">
-            <Link to="/">Home</Link>
-            <Link to="/movies/top_100">Top Movies</Link>
-            <Link to="/series/top_100">Top Series</Link>
-            <Link to="/myList">My List</Link>
+            <div id="nav-container">
+                <Link to="/">Home</Link>
+                <Link to="/movies/top_100">Top Movies</Link>
+                <Link to="/series/top_100">Top Series</Link>
+                <Link to="/myList">My List</Link>
+            </div>     
         </nav>
     )
 }
