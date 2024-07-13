015dc230e058fe5c28bc753d36b333f335f8cf38
Christian Peraza <christianperaza2000@gmail.com>
2024-07-12 23:02:58 -0300

    React Responsive Carousel Installed

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 import "../css/Home.css"
 
 export const Home = () => {
+
+    const [ popularMovies, setPopularMovies ] = useState([])
     
     useEffect(() => {
         fetch('https://api.themoviedb.org/3/movie/popular?api_key=3635f4a229260177fe2b5498e1ba967e&language=en-US')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => setPopularMovies(data.results))
     }, [])
       
       
