045631f5cf555aadd12871f62fc1656e1d574bf6
Christian Peraza <christianperaza2000@gmail.com>
2024-07-12 22:13:23 -0300

    Home Page Created

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { Header } from './components/Header'
+import { Home } from './pages/Home'
 
 import './css/App.css'
 
@@ -11,7 +12,7 @@ function App() {
         <Header />
 
         <Routes>
-          <Route index element={<h1>Home Page</h1>} />
+          <Route index element={<Home />} />
           <Route path="review/:id" element={<h1>Detail Page Review</h1>} />
           <Route path="movies/:type" element={<h1>Top 100 Movies</h1>} />
           <Route path="series/:type" element={<h1>Top 100 Series</h1>} />
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+import "../css/Home.css"
+
+export const Home = () => {
+    return (
+        <>
+            Home Page
+        </>
+    )
+}
