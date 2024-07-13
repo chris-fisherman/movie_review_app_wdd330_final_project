5c48428628a4899f9715c9ae872537bba3b2a828
Christian Peraza <christianperaza2000@gmail.com>
2024-07-12 21:33:43 -0300

    Header Reorganized Again

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from "react"
 import { Link } from "react-router-dom"
 
-import '../../css/Header.css'
+import '../css/Header.css'
 
 export const Header = () => {
     return (
         <header id="header">
             <div id="headerMain-container">
-                <Link to="/"><img id="headerMain-logo" src="../../assets/bella_vista_logo_complete.webp" alt="Logo" /></Link>
+                <Link to="/"><img id="headerMain-logo" src="../assets/bella_vista_logo_complete.webp" alt="Logo" /></Link>
 
                 <h2 id="headerMain-search">Search</h2>
             </div>
