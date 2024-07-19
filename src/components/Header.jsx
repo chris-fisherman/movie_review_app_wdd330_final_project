a00ab94a4669498a210aca455fd62f7e40f47602
Christian Peraza <christianperaza2000@gmail.com>
2024-07-19 13:38:39 -0300

    Logo Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,14 +2,18 @@ import React from "react"
 import { Link } from "react-router-dom"
 
 import { Nav } from "./Nav"
+import logo from "../assets/play_wave_logo.png"
 
 import '../css/Header.css'
+
 
 export const Header = () => {
     return (
         <header id="header">
             <div id="headerMain-container">
-                <Link to="/" id="headerMain-logo"><h2>Logo</h2></Link>
+                <Link to="/" id="headerMain-logo">
+                    <img src={logo} alt="Logo" width="30" />
+                </Link>
 
                 <h2 id="headerMain-search">Search</h2>
             </div>
